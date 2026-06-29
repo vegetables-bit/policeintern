ae931b218dd654ca269f93e6e8da8746134a82af
Andrew Masamba <andrewmasamba32@gmail.com>
2026-06-29 23:02:38 +0200

    Configure database for deployment

--- intern_system/intern_system/settings.py
+++ intern_system/intern_system/settings.py
@@ -84,10 +84,9 @@ WSGI_APPLICATION = 'intern_system.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 
